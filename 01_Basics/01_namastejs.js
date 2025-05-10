6ada4ffe28dbc8d5084d282a051a8533cbc89648
StarFire1701 <projectscollege.1701@gmail.com>
2025-05-10 10:20:36 +0000

    Namaste Javascript Created

---
+++ 01_Basics/01_namastejs.js
@@ -0,0 +1,8 @@
+// This file is just for printing namaste duniya in javascript
+console.log("Namaste duniya")
+
+/*
+In javascript there is no role of semicolon , the line runs with or without semicolon
+*/
+console.log("With Semicolon:Namaste Duniya");
+console.log("Without Semicolon:Namaste Duniya")
